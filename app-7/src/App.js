7bb112fdfde4ef403447fef2fd4a2e76e02cefb2
Chandler Willman <Chandler@Chandlers-MacBook-Pro-3.local>
2019-02-02 16:19:00 -0700

    finish app-7

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NewTask from './NewTask';
+import List from './List';
+
+/* Questions
+
+1. When do I need to include value and type on an input?
+2. What does .bind do? Why do we include it in the constructor?
+3. What does ... do?
+4. What are props?
+5. Why do we use () for return? What's actually happening here?
+
+
+*/
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state ={
+      list: [],
+    }
+
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+  handleAddTask(task){
+    this.setState({ list: [...this.state.list, task] });
+  }
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <NewTask add={this.handleAddTask} />
+        <List tasks={this.state.list} />
+
       </div>
     );
   }
